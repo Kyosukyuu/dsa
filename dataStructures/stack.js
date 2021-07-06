39f77bff03361937e6e4e250161101bbc5b4b260
Kyosukyuu <82899136+Kyosukyuu@users.noreply.github.com>
2021-07-06 11:07:52 -0400

    added stack with helper funcs

---
+++ dataStructures/stack.js
@@ -0,0 +1,24 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    this.items.push(item);
+  }
+  pop() {
+    return this.items.pop();
+  }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  get size() {
+    return this.items.length;
+  }
+}
+
+const stack = new Stack();
+stack.push(9);
+stack.push(100);
+stack.push(-4);
+stack.push(0);
+console.log(stack.size);
