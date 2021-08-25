53025ef4b71a2340b0dd56a48cb0e4b3b9422e60
Kyosukyuu <82899136+Kyosukyuu@users.noreply.github.com>
2021-08-25 14:25:19 -0400

    added jump search

---
+++ algorithms/jumpSearch.js
@@ -0,0 +1,30 @@
+const jumpSearch = (arr, target) => {
+  const arrLen = arr.length;
+  const step = Math.floor(Math.sqrt(arrLen));
+
+  if (arr[arrLen - 1] < target) return -1;
+
+  let subsetStart = 0;
+  let subsetEnd = step;
+
+  while (arr[Math.min(subsetEnd, arrLen) - 1] < target) {
+    console.log(subsetStart, "to", subsetEnd);
+    subsetStart = subsetEnd;
+    subsetEnd += step;
+  }
+  console.log(subsetStart, "to", subsetEnd);
+
+  while (arr[subsetStart] < target) {
+    subsetStart++;
+
+    if (subsetStart == Math.min(subsetEnd, arrLen)) return -1;
+  }
+
+  if (arr[subsetStart] == target) return subsetStart;
+  else return -1;
+};
+
+const arr = [1, 3, 5, 8, 10, 14, 23, 37, 40];
+
+const res = jumpSearch(arr, 23);
+console.log(`INDEX: `, res);
