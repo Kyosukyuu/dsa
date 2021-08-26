440b0988b4e41e645fb33493a941516f9c324216
Kyosukyuu <82899136+Kyosukyuu@users.noreply.github.com>
2021-08-25 23:14:34 -0400

    updated equal signs

--- algorithms/jumpSearch.js
+++ algorithms/jumpSearch.js
@@ -8,23 +8,25 @@ const jumpSearch = (arr, target) => {
   let subsetEnd = step;
 
   while (arr[Math.min(subsetEnd, arrLen) - 1] < target) {
-    console.log(subsetStart, "to", subsetEnd);
+    console.log(subsetStart, "to", subsetEnd - 1);
     subsetStart = subsetEnd;
     subsetEnd += step;
   }
-  console.log(subsetStart, "to", subsetEnd);
+  console.log(subsetStart, "to", subsetEnd - 1);
 
   while (arr[subsetStart] < target) {
     subsetStart++;
 
-    if (subsetStart == Math.min(subsetEnd, arrLen)) return -1;
+    if (subsetStart === Math.min(subsetEnd, arrLen)) return -1;
   }
 
-  if (arr[subsetStart] == target) return subsetStart;
+  if (arr[subsetStart] === target) return subsetStart;
   else return -1;
 };
 
-const arr = [1, 3, 5, 8, 10, 14, 23, 37, 40];
+const arr = [1, 3, 5, 8, 10, 14, 23, 37, 40, 60, 64, 79];
+//           0, 1, 2, 3, 4,  5,  6,  7,  8,  9,  10, 11
 
-const res = jumpSearch(arr, 23);
+const res = jumpSearch(arr, 79);
 console.log(`INDEX: `, res);
+console.log("ARR LEN:", arr.length);
